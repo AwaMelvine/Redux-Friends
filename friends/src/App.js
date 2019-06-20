c312c92c123619fc78fd432747b17e9596ff1b93
Awa Melvine <melvineawa@gmail.com>
2019-06-20 14:46:08 +0100

    Create login route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Route to="/login" component={Login} />
     </div>
   );
 }
