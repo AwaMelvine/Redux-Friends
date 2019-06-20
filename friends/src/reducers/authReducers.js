a83adb79a2c7d70b82d9eac94db8d93600e36049
Awa Melvine <melvineawa@gmail.com>
2019-06-20 14:25:29 +0100

    Create auth reducer

---
+++ friends/src/reducers/authReducers.js
@@ -0,0 +1,31 @@
+import { LOGGING_IN, LOGIN_SUCCESS, LOGIN_FAILURE } from "../actions/types";
+
+const initialState = {
+  loggingIn: false,
+  error: null,
+  loggedIn: false
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case LOGGING_IN:
+      return {
+        ...state,
+        loggingIn: action.payload
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loggedIn: action.payload
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        loggedIn: false
+      };
+
+    default:
+      break;
+  }
+};
